16e8ae8426010f5050bd1cd10ce2fa8cd82f1a6d
dawood6 <dawoodsadiq171@gmail.com>
2020-12-04 20:20:34 +0500

    project-12c

--- src/pages/data.js
+++
@@ -1,9 +0,0 @@
-const project = {
-    "project01": {
-        "project_url": "",
-        "repo_url": "",
-        "project_img": "",
-        "project_title": "",
-        
-    }
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,11 @@
-import React from "react";
-import { useQuery, useMutation } from "@apollo/client";
-import { READ_OPERATION, REMOVE_OPERATION } from "../graphql/graphql";
-import "./main.css";
-import { AddNew } from "./AddNew";
-import FlipMove from "react-flip-move";
-import Loader from "./Loader";
-
-export default function Home() {
-  const { loading, error, data } = useQuery(READ_OPERATION);
-  const [removeRecord] = useMutation(REMOVE_OPERATION);
-  let removeData = async (e) => {
-    removeRecord({
-      variables: {
-        id: e,
-      },
-      refetchQueries: [{ query: READ_OPERATION }],
-    });
-  };
-  if (loading) {
-    return <Loader />;
-  }
-  console.log(error);
-  if (error) {
-    return <div>error ...</div>;
-  }
-  const listItems = data.read.map((d) => {
+import React from 'react'
+import Home from '../components/index'
+const index = () => {
     return (
-      <div className="list" key={d.id}>
-        <p>
-          <input type="text" disabled value={d.todo} />
-          <span>
-            <span className="i" onClick={() => removeData(d.id)}>
-              🗑️
-            </span>
-          </span>
-        </p>
-      </div>
-    );
-  });
-  return (
-    <div className="App">
-      <div>
-        <h1>Project 12C</h1>
-        <AddNew />
-        <FlipMove duration={300} easing="ease-in-out">
-          {listItems}
-        </FlipMove>
-      </div>
-    </div>
-  );
+        <div>
+            <Home />
+        </div>
+    )
 }
+
+export default index
